3324d60633805cae2b9a271800c79084f7ffabb1
某10 <0w0@0w0.pw>
2017-02-25 13:51:07 +0800

    默认32线程拖他娘的P站!!!
    鬼知道会不会被Ban

--- multi_test/main.py
+++ multi_test/main.py
@@ -4,22 +4,20 @@ from multiprocessing.dummy import Pool as ThreadPool
 from multiprocessing import Lock
 from pixivpy3 import *
 
-user="yourusername@gmail.com"
-pwd="passwd"
-path='./downloads/'
-max_thread=8
+user = "Your@Pixiv.net"  #你的Pixiv登陆邮箱/用户名
+pwd = "YourPassword"          #你的Pixiv登陆密码
+path = './downloads/pixiv/' #下载位置
+max_thread = 32             #最大线程数量
 
 if not os.path.exists(path):
     os.makedirs(path)
+
+lock = Lock()
 api = PixivAPI()
 token = api.login(user, pwd)
-lock =Lock()
-illust_url_list=[]
-page_list=[]
-page_count =0
-pub=''
-pg_result = None
 
+illust_url_list = []
+#读取作品下载链接到illust_url_list
 def read_illust(pg):
     global pub,illust_url_list,page_count
     with lock:
@@ -36,7 +34,6 @@ def read_illust(pg):
         else:
             with lock:
                 illust_url_list.append(i.work.image_urls.large)
-    
 
 download_count=0;
 def download_illust(illust_url):
@@ -45,19 +42,25 @@ def download_illust(illust_url):
         download_count +=1
         print("downloading %d/%d"%(download_count,len(illust_url_list)),end='\r')
     api.download(illust_url,path)
-#获取所有图片链接
 
-pub='pubilc'
-pg_result = api.me_favorite_works(publicity=pub)
-for i in range(pg_result.pagination.pages): page_list.append(i+1)
-
-pool = ThreadPool(max_thread)
-data = pool.map(read_illust, page_list)
-pool.close()
-pool.join()
-print('\n',end='\r')
+fav_list = ['pubilc','private']
+page_count = 0
+page_list = None
+pub = None
+for k in fav_list:
+    page_count = 0#清空页码计数器
+    page_list = []#清空页码列表
+    pub = k
+    pg_result = api.me_favorite_works(publicity = pub)
+    for i in range(pg_result.pagination.pages):
+        page_list.append(i+1)
+    pool = ThreadPool(max_thread)
+    pool.map(read_illust, page_list)
+    pool.close()
+    pool.join()
+    print('\n',end='\r')
 pool = ThreadPool(max_thread)
 data = pool.map(download_illust, illust_url_list)
 pool.close()
 pool.join()
-print('\n',end='\r')
+print('\n下载完毕')
